a457c5e28e2ee3716d4c84babdf475bd990d20e4
toxa4elovek <toxa4elovek@gmail.com>
2017-08-23 07:03:31 +0300

    fix setting v 1.0.0

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -14,6 +14,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'baseUrl' => '',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -36,14 +37,15 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
